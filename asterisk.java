556d04d1f8dd189aa914334c2e3f2f3132ca4638
Eda Akyıl <edaakyil@gmail.com>
2023-08-23 15:47:44 +0300

    Create asterisk.java

---
+++ asterisk.java
@@ -0,0 +1,40 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int i, k, j, size, row, column;
+		size = 6;
+		
+		/* 1. Right Triangle Star Pattern */
+		// for(i = 0; i < size; i++) {
+		//         for(k = 0; k <= i; k++)
+		//                 System.out.print("*");
+	        //         System.out.println();
+                // }
+                
+
+                /* 2. Left Triangle Star Pattern */
+                // for(i = 0; i < size; i++) {
+                //         for(k = 0; k < size - (i + 1); k++)
+                //                 System.out.print("  ");
+                //         for(k = 0; k <= i; k++)
+                //                 System.out.print(" *");
+                //         System.out.println();
+                // }
+                
+                
+                /* 3. Pyramid Star Pattern */
+                // for(i = 0; i < size; i++) {
+                //         for(k = 0; k < size - i; k++)
+                //                 System.out.print(" ");
+                //         for(k = 0; k <= i; k++)
+                //                 System.out.print("* ");
+                //         System.out.println();
+                // }
+                
+                /* 4. Diamond Shape Pattern */
+                for()
+                
+   
+                
+	}
+}
